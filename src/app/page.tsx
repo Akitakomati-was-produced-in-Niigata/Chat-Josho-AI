808b3c50732a0d8063c5680a64bfd8f22c94f7c5
Akitakomati-was-produced-in-Niigata <omoomochi34@yahoo.co.jp>
2024-09-17 15:10:55 +0900

    login完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,18 @@ import { use, useState } from "react";
 
 export default function Home() {
   const [text, setText] = useState("");
-  const [content, setContent] = useState<string>('おこたえします');
-  const sendtext = text 
-
+  const [content1, setContent1] = useState<string>('お答えします');
+  const [content2, setContent2] = useState<string>('')
   const getAzData = async () => {
     
     try {
-      console.log('start',sendtext);
-      const response = await fetch(`http://localhost:3000/api/azopenai?message=${encodeURIComponent(sendtext)}`);
+      console.log('start',text);
+      const response = await fetch(`http://localhost:3000/api/azopenai?message=${encodeURIComponent(text)}`);
       if (response.status !== 200) throw new Error('Failed to fetch tasks');
         const data = await response.json();
         console.log('途中 data : ', data);
-        setContent(data[0].message.content); 
+        setContent1("");
+        setContent2(data[0].message.content) 
         console.log("終わり");  
     } catch (err) {
         console.log('🚀 ~ file: index.tsx:32 ~ getAzData ~ err:', err);
@@ -29,7 +29,7 @@ export default function Home() {
     <div>
       <h1 className="center">Chat JOSHO AI</h1>
       <div className="div">
-      <Link href={"/Login"} className="login">＞ Go to Login</Link>
+      <Link href={"/Login"} className="link">＞ Go to Login</Link>
       <form>
         <input
           type="text"
@@ -37,15 +37,12 @@ export default function Home() {
           className="input"
           placeholder="聞きたいことを入力してください" required>
         </input>
-
-
         <button className="submit" type="button" onClick={getAzData}>⇓</button>
-
       </form>  
-
-      </div>
+     </div>
       <div className="ai">
-      <p className="answer">{content}</p>
+      <p className="answer">{content1}</p>
+      <p className="answer2">{content2}</p>
       </div>
     </div>
   );
