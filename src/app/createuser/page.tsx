ff059a35d0c4bc00f4384327b321cf00f978aa98
Akitakomati-was-produced-in-Niigata <omoomochi34@yahoo.co.jp>
2024-10-08 14:31:37 +0900

    aaa

--- src/app/createuser/page.tsx
+++ src/app/createuser/page.tsx
@@ -1,7 +1,13 @@
 export default function Createuser() {
-    return(
-        <div>
-            <p>Let's create user</p>
-        </div>
-    );
+  return(
+    <div className="createcontainer">
+      <p>Let you create user</p>
+      <form>
+        <p className="font">ユーザー作成</p>
+        <input className="createname" type="text" placeholder="Username" />
+        <input className="createpass" type="password" placeholder="Password" />
+        <button className="login2" type="submit">ログイン</button>
+      </form>
+    </div>
+  );
 }
